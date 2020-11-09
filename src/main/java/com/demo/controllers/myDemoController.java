c08c6982361a50408fec6a3496d438693528b7b7
Bhagyalakshmi U <b.mattakkarau@dxc.com>
2020-11-10 00:27:35 +0530

    First Commit

--- src/main/java/com/demo/controllers/myDemoController.java
+++ src/main/java/com/demo/controllers/myDemoController.java
@@ -1,16 +1,18 @@
 package com.demo.controllers;
 
-import java.io.FileOutputStream;
+//import java.io.FileOutputStream;
 
 //import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 //import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.multipart.MultipartFile;
+
+import com.demo.model.Account;
 
 //import com.demo.model.Account;
 
@@ -19,25 +21,45 @@ import org.springframework.web.multipart.MultipartFile;
 public class myDemoController 
 {
 
-/*	@RequestMapping(value="/createAccount")
-	public String createAccount(@Valid @ModelAttribute ("aNewAccount") Account account, BindingResult result)
+@RequestMapping(value="/createAccount")
+	//public String createAccount(@Valid @ModelAttribute ("aNewAccount") Account account, BindingResult result)
+public String createAccount(@ModelAttribute ("aNewAccount") Account account)
 	{
-		if(result.hasErrors())
+		/*if(result.hasErrors())
 		{
 			System.out.println("Form has Errors");
 			return "createAccount";
-		}
+		}*/
 		System.out.println(account.getFirstName() +" "+ account.getLastName()+ " "+account.getAddress()+" "+account.getEmail()+ " "+account.getAge());
 		return "createAccount";
+	
 	}
 
-	@RequestMapping(value="/accountCreated",method=RequestMethod.POST)
+		@RequestMapping(value="/doCreate")
+	public String doCreate(@ModelAttribute ("aNewAccount") Account account)
+	{
+		
+		System.out.println("Do create: New account info: "+account.getFirstName() +" "+ account.getLastName()+ " "+account.getAddress()+" "+account.getEmail()+ " "+account.getAge());
+		System.out.println("Do create: Going off and creating an actual account ");
+		return "redirect:accConfirm.html";
+	}
+		
+		@RequestMapping(value="/accConfirm")
+		public String accConfirm(@ModelAttribute ("aNewAccount") Account account)
+		{
+			
+			System.out.println("Account Confirmed: "+account.getFirstName() +" "+ account.getLastName()+ " "+account.getAddress()+" "+account.getEmail()+ " "+account.getAge());
+			
+			return "accConfirm";
+		}
+
+/*		@RequestMapping(value="/accountCreated",method=RequestMethod.POST)
 	public String performance(Account account)
 	{
 		
 		System.out.println(account.getFirstName() +" "+ account.getLastName()+ " "+account.getAddress()+" "+account.getEmail()+ " "+account.getAge());
 		return "accountCreated";
-	}*/
+	}
 	@RequestMapping(value="/myForm")
 	public String myForm()
 	{
@@ -66,5 +88,5 @@ public class myDemoController
 			System.out.println("Error saving file.");
 		}
 		return "operationComplete";
-	}
+	}*/
 }
